441f4d4cf2c73379243abf0004b90a7ca11fd745
ayush-garg341 <gargayush341@gmail.com>
2020-03-29 18:42:03 +0530

    dired package added to see project structure or directories.

--- init.el
+++ init.el
@@ -230,4 +230,22 @@
   (setq web-mode-code-indent-offset 2))
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
+
+;; Dired for project directory structure
+(setq dired-dwim-target t)
+
+(use-package dired-narrow
+:ensure t
+:config
+(bind-key "C-c C-n" #'dired-narrow)
+(bind-key "C-c C-f" #'dired-narrow-fuzzy)
+(bind-key "C-x C-N" #'dired-narrow-regexp)
+)
+
+(use-package dired-subtree :ensure t
+  :after dired
+  :config
+  (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map)
+  (bind-key "<backtab>" #'dired-subtree-cycle dired-mode-map))
+
 ;;; init.el ends here
